120260cbfb96ffbc5284e83bab3a68094fe2874d
Sachin m <sachin@signitysolutions.com>
2019-01-07 19:35:45 +0530

    initial commit

---
+++ OTP Login/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  OTP Login
+//
+//  Created by signity on 07/01/19.
+//  Copyright © 2019 SM. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+//        FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
+//        // Optional: Place the button in the center of your view.
+//        loginButton.center = self.view.center;
+//        [self.view addSubview:loginButton];
+    }
+
+
+}
+
